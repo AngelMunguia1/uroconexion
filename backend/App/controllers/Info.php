38b5079a209ff9f3a5b37cbcdaef5b2c713bbb72
AngelMunguia1 <angel.munguia@grupolahe.com>
2022-09-09 18:02:21 -0500

    init

---
+++ backend/App/controllers/Info.php
@@ -0,0 +1,14 @@
+<?php
+// The location of the PDF file
+// on the server
+$filename = "assets/pdf/info_uro.pdf";
+  
+// Header content type
+header("Content-type: application/pdf");
+  
+header("Content-Length: " . filesize($filename));
+  
+// Send the file to the browser.
+readfile($filename);
+//listo
+?>
